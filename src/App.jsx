9ad65d1f77fd0538b94ef06861f7ee1267272708
umangbhalodiya <umangbhalodiya660@gmail.com>
2023-03-01 18:44:59 +0530

    cmnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const orderPlace = () => {
+   const orderPlace = () => {
     console.log("order placed");
   };
 
